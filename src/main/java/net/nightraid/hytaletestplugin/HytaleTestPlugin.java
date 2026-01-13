47ae490fff9110f741b57c08cd37694f74fca6a1
Kielian <105607229+NadoHimself@users.noreply.github.com>
2026-01-14 00:00:18 +0100

    fix: Correct all Java compilation errors - use valid Hytale API

--- src/main/java/net/nightraid/hytaletestplugin/HytaleTestPlugin.java
+++ src/main/java/net/nightraid/hytaletestplugin/HytaleTestPlugin.java
@@ -1,59 +1,29 @@
 package net.nightraid.hytaletestplugin;
 
-import com.hypixel.hytale.server.core.HytaleServer;
-import com.hypixel.hytale.server.core.plugin.HytalePlugin;
-import com.hypixel.hytale.event.IEventDispatcher;
+import com.hypixel.hytale.server.core.plugin.AbstractPlugin;
 import net.nightraid.hytaletestplugin.listeners.PlayerListener;
 import net.nightraid.hytaletestplugin.listeners.BlockListener;
 
-public class HytaleTestPlugin extends HytalePlugin {
-
+public class HytaleTestPlugin extends AbstractPlugin {
+    
+    @Override
+    public void onLoad() {
+        getLogger().info("HytaleTestPlugin wird geladen...");
+    }
+    
     @Override
     public void onEnable() {
-        getLogger().info("HytaleTestPlugin enabled!");
+        getLogger().info("HytaleTestPlugin aktiviert!");
         
-        // Register Event Listeners
-        registerEvents();
+        // Listener registrieren
+        getEventBus().register(new PlayerListener(this));
+        getEventBus().register(new BlockListener(this));
+        
+        getLogger().info("Listener erfolgreich registriert!");
     }
-
+    
     @Override
     public void onDisable() {
-        getLogger().info("HytaleTestPlugin disabled!");
-    }
-
-    private void registerEvents() {
-        HytaleServer server = HytaleServer.get();
-        
-        // Register Player Events
-        PlayerListener playerListener = new PlayerListener(this);
-        
-        // AddPlayerToWorldEvent (PlayerJoinEvent equivalent)
-        IEventDispatcher dispatcher = server.getEventBus()
-            .dispatchFor(com.hypixel.hytale.server.core.event.events.player.AddPlayerToWorldEvent.class, "*");
-        if (dispatcher.hasListener()) {
-            dispatcher.register(playerListener::onPlayerJoin);
-        }
-        
-        // PlayerDisconnectEvent (PlayerQuitEvent equivalent)
-        dispatcher = server.getEventBus()
-            .dispatchFor(com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent.class, "*");
-        if (dispatcher.hasListener()) {
-            dispatcher.register(playerListener::onPlayerDisconnect);
-        }
-        
-        // PlayerChatEvent
-        dispatcher = server.getEventBus()
-            .dispatchFor(com.hypixel.hytale.server.core.event.events.player.PlayerChatEvent.class, "*");
-        if (dispatcher.hasListener()) {
-            dispatcher.register(playerListener::onPlayerChat);
-        }
-        
-        // Register Block Events
-        BlockListener blockListener = new BlockListener(this);
-        
-        // Note: ECS Events werden über ComponentAccessor.invoke() gefeuert
-        // Für ECS Events brauchst du ein System oder einen Entity-Listener
-        
-        getLogger().info("Event listeners registered!");
+        getLogger().info("HytaleTestPlugin deaktiviert!");
     }
 }
